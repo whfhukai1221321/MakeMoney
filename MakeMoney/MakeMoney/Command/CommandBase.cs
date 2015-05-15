af4431dd1fe2b9899f5d217d546815c84c347a24
stonecity <bluecart@live.com>
2015-05-15 22:57:03 +0800

    .

---
+++ MakeMoney/MakeMoney/Command/CommandBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeMoney.Command
+{
+    class CommandBase
+    {
+    }
+}
