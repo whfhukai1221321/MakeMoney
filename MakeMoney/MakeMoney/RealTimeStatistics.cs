f18896c5fa3a5c402bb04498600eb1949c35b6c3
stonecity <bluecart@live.com>
2015-05-15 23:53:45 +0800

    添加了波动告警

---
+++ MakeMoney/MakeMoney/RealTimeStatistics.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeMoney
+{
+    class RealTimeStatistics
+    {
+        public double AvgPrice { get; set; }
+
+        public int Count { get; set; }
+
+        public double Sum { get; set; }
+
+        public float Max { get; set; }
+
+        public float Min { get; set; }
+
+        public double CalcAvg()
+        {
+            if (this.Count > 0)
+            {
+                this.AvgPrice = this.Sum / this.Count;
+            }
+
+            return this.AvgPrice;
+        }
+
+        public double CalcDifferencMinMax()
+        {
+            return this.Max - this.Min;
+        }
+    }
+}
---
+++ MakeMoney/MakeMoney/VolatilityAlarm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeMoney
+{
+    /// <summary>
+    /// 股市趋势波动告警
+    /// 当股市的波动超过了预设的门限，发送告警信息
+    /// </summary>
+    class VolatilityAlarm
+    {
+        /// <summary>
+        /// 告警门限值
+        /// </summary>
+        public float Threshold = 0.2F;
+
+        public RealTimeStatistics Statistics { get; set; }
+
+        public IList<StockLiveData> StockLiveDatas = new List<StockLiveData>();
+
+        public void Calculate(float currentPrice)
+        {
+            if (this.Statistics == null)
+            {
+                this.Statistics = new RealTimeStatistics();
+            }
+
+            var difference = this.Statistics.AvgPrice - currentPrice;
+            var upDownDiff = this.Statistics.CalcDifferencMinMax();
+            if (upDownDiff > 0)
+            {
+                var ratio = difference / upDownDiff;
+                if (ratio <= Threshold)
+                {
+                    // 最新价格与平均值的差值小于了最高点最低值差值的20%比例，进行告警
+
+                }
+            }
+
+            this.Statistics.Count += 1;
+            this.Statistics.Sum += currentPrice;
+
+            if (this.Statistics.Min > currentPrice)
+            {
+                this.Statistics.Min = currentPrice;
+            }
+
+            if (this.Statistics.Max < currentPrice)
+            {
+                this.Statistics.Max = currentPrice;
+            }
+
+            this.Statistics.CalcAvg();
+        }
+    }
+}
